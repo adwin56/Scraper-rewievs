4f61a6f65fefbb02ea76a371ab7567147a16ef11
adwin56 <andrejspasennikov6470@gmail.com>
2024-08-09 02:53:24 +0500

    message:test

--- parse_reviews_yandex.php
+++ parse_reviews_yandex.php
@@ -6,8 +6,6 @@ error_reporting(E_ALL);
 // Укажите URL страницы с отзывами на Яндекс Картах
 $url = 'https://yandex.ru/maps/org/daymond_klinik/164798670887/reviews/?indoorLevel=1&ll=49.123700%2C55.790012&z=16';
 
-echo 'Начинаю парсинг URL: ' . $url . PHP_EOL;
-
 // Получение HTML-кода страницы
 $html = file_get_contents($url);
 
@@ -15,8 +13,6 @@ if ($html === FALSE) {
     echo json_encode(['error' => 'Не удалось загрузить страницу']);
     exit;
 }
-
-echo 'HTML получен, длина: ' . strlen($html) . PHP_EOL;
 
 // Создание объекта DOMDocument
 $dom = new DOMDocument();
@@ -34,8 +30,6 @@ if ($reviewNodes === FALSE) {
     exit;
 }
 
-echo 'Найдено отзывов: ' . $reviewNodes->length . PHP_EOL;
-
 foreach ($reviewNodes as $node) {
     $author = $xpath->query(".//div[contains(@class, 'business-review-view__author-name')]/a/span", $node)->item(0)->textContent ?? 'Неизвестный автор';
     $date = $xpath->query(".//span[contains(@class, 'business-review-view__date')]", $node)->item(0)->textContent ?? 'Дата отсутствует';
@@ -48,13 +42,9 @@ foreach ($reviewNodes as $node) {
         'rating' => (int)$rating,  // Приведение к числу
         'reviewText' => trim($reviewText)
     ];
-
-    echo 'Обработан отзыв от автора: ' . $author . PHP_EOL;
 }
 
 // Возврат данных в формате JSON
 header('Content-Type: application/json');
 echo json_encode($reviews);
-
-echo 'Парсинг завершен, отправка данных в формате JSON' . PHP_EOL;
 ?>
