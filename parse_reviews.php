da2c3fcdbe11b44a7d5f237b2d5bf87b2490adb8
adwin56 <andrejspasennikov6470@gmail.com>
2024-08-12 12:50:55 +0500

    12

--- parse_reviews.php
+++ parse_reviews.php
@@ -1,75 +1,76 @@
 <?php
-// URL страницы с отзывами на 2ГИС
-$url = 'https://2gis.ru/n_novgorod/inside/2674647933966364/firm/70000001021080960/tab/reviews?m=43.999939%2C56.311007%2F16%2Fp%2F0.3';
+while (true) {
+    // Получаем текущее время
+    $currentHour = (int)date('H');
+    $currentMinute = (int)date('i');
 
-// Получение HTML-кода страницы
-$html = file_get_contents($url);
+    // Проверяем, наступила ли полночь
+    if ($currentHour === 0 && $currentMinute === 0) {
+        // Ваш код для парсинга 2ГИС
+        $url = 'https://2gis.ru/n_novgorod/inside/2674647933966364/firm/70000001021080960/tab/reviews?m=43.999939%2C56.311007%2F16%2Fp%2F0.3';
+        $html = file_get_contents($url);
 
-if ($html === FALSE) {
-    echo json_encode(['error' => 'Не удалось загрузить страницу']);
-    exit;
-}
-
-// Создание объекта DOMDocument
-$dom = new DOMDocument();
-@$dom->loadHTML($html);
-
-// Создание объекта XPath для удобного парсинга
-$xpath = new DOMXPath($dom);
-
-// Парсинг отзывов
-$reviews = [];
-$totalRating = 0;
-$reviewCount = 0;
-$reviewNodes = $xpath->query("//div[contains(@class, '_11gvyqv')]");
-
-foreach ($reviewNodes as $node) {
-    $author = $xpath->query(".//span[@class='_16s5yj36']", $node)->item(0)->textContent ?? 'Неизвестный автор';
-    $date = $xpath->query(".//div[@class='_4mwq3d']", $node)->item(0)->textContent ?? 'Дата отсутствует';
-    $rating = $xpath->query(".//div[@class='_1fkin5c']/span", $node)->length ?? 0;
-    $reviewText = $xpath->query(".//a[@class='_ayej9u3']", $node)->item(0)->textContent ?? 'Текст отсутствует';
-    
-    // Парсинг аватарок
-    $avatarUrl = 'Аватар отсутствует'; // Значение по умолчанию
-    $avatarNodeImage = $xpath->query(".//div[contains(@class, '_1dk5lq4')]", $node)->item(0);
-    $avatarNodeText = $xpath->query(".//div[contains(@class, '_10v2ue3')]", $node)->item(0);
-    
-    if ($avatarNodeImage) {
-        // Если аватарка с фоновым изображением
-        $style = $avatarNodeImage->getAttribute('style');
-        if (preg_match('/background-image:url\(([^)]+)\)/', $style, $matches)) {
-            $avatarUrl = $matches[1];
+        if ($html === FALSE) {
+            echo json_encode(['error' => 'Не удалось загрузить страницу']);
+            exit;
         }
-    } elseif ($avatarNodeText) {
-        // Если аватарка с текстом
-        $avatarUrl = $avatarNodeText->textContent;
+
+        $dom = new DOMDocument();
+        @$dom->loadHTML($html);
+        $xpath = new DOMXPath($dom);
+
+        $reviews = [];
+        $totalRating = 0;
+        $reviewCount = 0;
+        $reviewNodes = $xpath->query("//div[contains(@class, '_11gvyqv')]");
+
+        foreach ($reviewNodes as $node) {
+            $author = $xpath->query(".//span[@class='_16s5yj36']", $node)->item(0)->textContent ?? 'Неизвестный автор';
+            $date = $xpath->query(".//div[@class='_4mwq3d']", $node)->item(0)->textContent ?? 'Дата отсутствует';
+            $rating = $xpath->query(".//div[@class='_1fkin5c']/span", $node)->length ?? 0;
+            $reviewText = $xpath->query(".//a[@class='_ayej9u3']", $node)->item(0)->textContent ?? 'Текст отсутствует';
+
+            $avatarUrl = 'Аватар отсутствует';
+            $avatarNodeImage = $xpath->query(".//div[contains(@class, '_1dk5lq4')]", $node)->item(0);
+            $avatarNodeText = $xpath->query(".//div[contains(@class, '_10v2ue3')]", $node)->item(0);
+
+            if ($avatarNodeImage) {
+                $style = $avatarNodeImage->getAttribute('style');
+                if (preg_match('/background-image:url\(([^)]+)\)/', $style, $matches)) {
+                    $avatarUrl = $matches[1];
+                }
+            } elseif ($avatarNodeText) {
+                $avatarUrl = $avatarNodeText->textContent;
+            }
+
+            $totalRating += $rating;
+            $reviewCount++;
+
+            $reviews[] = [
+                'author' => trim($author),
+                'date' => trim($date),
+                'rating' => $rating,
+                'reviewText' => trim($reviewText),
+                'avatarUrl' => $avatarUrl,
+                'platform' => '2ГИС'
+            ];
+        }
+
+        $averageRating = $reviewCount > 0 ? $totalRating / $reviewCount : 0;
+        $jsonData = [
+            'reviews' => $reviews,
+            'averageRating2GIS' => round($averageRating, 1)
+        ];
+
+        file_put_contents('parse_2GIS.json', json_encode($jsonData, JSON_PRETTY_PRINT));
+
+        echo json_encode($jsonData);
+        
+        // Задержка на одну минуту, чтобы избежать повторного запуска скрипта в ту же минуту
+        sleep(60);
     }
 
-    $totalRating += $rating;
-    $reviewCount++;
-
-    $reviews[] = [
-        'author' => trim($author),
-        'date' => trim($date),
-        'rating' => $rating,
-        'reviewText' => trim($reviewText),
-        'avatarUrl' => $avatarUrl,
-        'platform' => '2ГИС'
-    ];
+    // Задержка на 30 секунд перед повторной проверкой времени
+    sleep(30);
 }
-
-// Расчет среднего рейтинга
-$averageRating = $reviewCount > 0 ? $totalRating / $reviewCount : 0;
-
-// Формирование данных для JSON
-$jsonData = [
-    'reviews' => $reviews,
-    'averageRating2GIS' => round($averageRating, 1) // Средний рейтинг с одной цифрой после запятой
-];
-
-// Запись данных в файл
-file_put_contents('parse_2GIS.json', json_encode($jsonData, JSON_PRETTY_PRINT));
-
-// Возврат данных в формате JSON
-header('Content-Type: application/json');
-echo json_encode($jsonData);
+?>
